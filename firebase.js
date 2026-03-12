47b3c05b8165341ce9eb88ddfd26d0b5d91c43c8
kzkz-dev <amkkawchur@gmail.com>
2026-03-13 04:09:56 +0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+// Storage will be used in v4.2+
+// import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsbZ1fqDivv8OyUiTcaEMcpZlJlM1TI6Y",
+  authDomain: "fluxgram-87009.firebaseapp.com",
+  projectId: "fluxgram-87009",
+  storageBucket: "fluxgram-87009.firebasestorage.app",
+  messagingSenderId: "698836385253",
+  appId: "1:698836385253:web:c40e67ee9006cff536830c"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+// export const storage = getStorage(app);
