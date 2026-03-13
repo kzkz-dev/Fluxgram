456b30558e7e2e5c7ad611d8bf2f5dd93801481c
kzkz-dev <amkkawchur@gmail.com>
2026-03-13 17:29:57 +0600

    Update calls.js

--- calls.js
+++ calls.js
@@ -1,12 +1,12 @@
 export const CallsModule = {
-  // v4.1 foundation only.
-  // Existing call system can be moved here in next patch.
   startCall(type) {
     console.log("Start call:", type);
   },
+
   acceptCall() {
     console.log("Accept call");
   },
+
   endCall() {
     console.log("End call");
   }
